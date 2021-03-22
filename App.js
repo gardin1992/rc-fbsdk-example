ad0256701fb42e208f29eb5d4ec1c45d5640b800
Joao Gardin <gardin1992@gmail.com>
2021-03-22 17:21:41 -0300

    teste android login okay

--- App.js
+++ App.js
@@ -1,12 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { LoginButton, AccessToken } from 'react-native-fbsdk';
 
 export default function App() {
+  const [token, setToken] = useState(null);
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <LoginButton
+        onLoginFinished={(error, result) => {
+          if (error) {
+            console.log("login has error: " + result.error);
+          } else if (result.isCancelled) {
+            console.log("login is cancelled.");
+          } else {
+            AccessToken.getCurrentAccessToken().then(data => {
+              setToken(data.accessToken.toString());
+            })
+          }
+        }}
+        onLogoutFinished={() => setToken(null)}
+      />
+      {token ? <Text style={{ textAlign: 'center' }}>{`Token: ${token}`}</Text> : null}
     </View>
   );
 }
